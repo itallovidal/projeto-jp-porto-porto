62d6911fea94112809ea78d3e82255ba137ace0a
italloVidal <itallovidalll@gmail.com>
2024-11-19 16:45:43 -0300

    some logo changes

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -2,24 +2,19 @@ import LogoClouds from "@/components/logoClouds";
 import Contact from '@/components/contact'
 import Services from '@/components/services'
 import BannerPorto from "@/components/bannerPorto";
-import Selling from "@/components/selling";
 import Accordion from "@/components/accordion";
 
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
+  title: 'Porto & Porto',
+  description: 'profissionais altamente qualificados no segmento de venda automotiva, transporte de Veículos, locação de veículos, equipamentos pesados e carretas rodoviárias.',
 }
 
 
 export default function Home() {
-
-    console.log("a")
-
   return (
     <>
         <BannerPorto/>
         <Contact />
-        {/*<Selling/>*/}
         <Accordion/>
         <Services />
         <LogoClouds/>
